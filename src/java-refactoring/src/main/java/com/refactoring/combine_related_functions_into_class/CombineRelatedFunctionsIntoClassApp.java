6a936785238878999c63e8f2113478ba1c6aaca9
leogtzr <leogutierrezramirez@gmail.com>
2019-11-10 16:22:50 -0700

    add combine related functions into class

---
+++ src/java-refactoring/src/main/java/com/refactoring/combine_related_functions_into_class/CombineRelatedFunctionsIntoClassApp.java
@@ -0,0 +1,28 @@
+package com.refactoring.combine_related_functions_into_class;
+
+public class CombineRelatedFunctionsIntoClassApp {
+
+    private static final class Window {  }
+
+    private static final class SomeClass {
+
+        // TODO; fix this ...
+        private static void resize(final Window window, final int x, final int y) {
+            // ...
+        }
+
+        private static void maximize(final Window window) {
+
+        }
+
+        private static void destroy(final Window window) {
+
+        }
+
+
+    }
+
+    public static void main(final String[] args) {
+
+    }
+}
