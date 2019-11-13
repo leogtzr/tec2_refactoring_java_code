389d441ff6806bae26d7574a9d167df3936a44d7
leogtzr <leogutierrezramirez@gmail.com>
2019-11-13 14:54:18 -0700

    add replace constructor with factory method

---
+++ src/java-refactoring/src/main/java/com/refactoring/replace_constructor_with_factory_function/ReplaceConstructorWithFactoryMethodApp.java
@@ -0,0 +1,7 @@
+package com.refactoring.replace_constructor_with_factory_function;
+
+public class ReplaceConstructorWithFactoryMethodApp {
+    public static void main(final String[] args) {
+
+    }
+}
---
+++ src/java-refactoring/src/main/java/com/refactoring/replace_constructor_with_factory_function/domain/Button.java
@@ -0,0 +1,16 @@
+package com.refactoring.replace_constructor_with_factory_function.domain;
+
+public class Button {
+
+    public Button() {}
+
+    public void onClick() {
+
+    }
+
+    public void render() {
+
+    }
+
+    // TODO: add code here ...
+}
